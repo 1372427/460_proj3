3c4ee417db9c65715b8924089ae440c059984919
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-04-21 15:09:59 -0400

    year animation and styling for moving divs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+
+To make the svg the size of the window
 https://stackoverflow.com/questions/1038727/how-to-get-browser-width-using-javascript-code 
+
+To get the animation on year
+http://bl.ocks.org/darrenjaworski/5544599
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let w = getWidth()-20;
 let h = getHeight()-20;
-let h1 = 20;
-let h2 = getHeight() - 200;
-let svg;
-let floatingDiv;
+let h1 = 0;
+let h2 = getHeight();
+let svg, map;
+let stateValues;
+let floatingDiv, floatingDiv2;
+let running =false, chartType, timer, button, year;
+let clearAnimator;
 
 //getWidth and getHeight from https://stackoverflow.com/questions/1038727/how-to-get-browser-width-using-javascript-code
 function getWidth() {
@@ -30,7 +33,11 @@ function getHeight() {
 const rowConverter = (d) => {
     return {
     year:  d3.timeParse('%Y')(+d.YEAR), 
-    state: d.STATE.toLowerCase()
+    state: d.STATE.toLowerCase(),
+    enroll: parseInt(d.ENROLL),
+    revenue: parseFloat(d.TOTAL_REVENUE),
+    expend: parseFloat(d.TOTAL_EXPENDITURE),
+    grade_all: parseInt(d.GRADES_ALL_G)
 }
 }
 
@@ -40,30 +47,70 @@ handleUpdate(Math.log(d3.event.transform.k))
 }
 
 const update1 = (d) => {
+  if(chartType==1)return;
+  chartType=1;
+  let variable = "enroll";
+
   floatingDiv.innerText = "1"
+  floatingDiv.classList.remove('hidden')
+  floatingDiv2.classList.add('hidden')
+      //Bind data and create one path per GeoJSON feature
+      map.selectAll(".state")
+      .style('stroke', 'green')
+      .style("fill", d => {
+        console.log(stateValues.get(d.properties.name))
+      
+      //If value is undefined…
+      return "#ccc";
+      
+      });
+  
   }
   
   const update2 = (d) => {
+    if(chartType==2)return;
+    chartType=2;
+    let variable = "revenue";
   
-    floatingDiv.innerText = "2"
+    floatingDiv2.innerText = "2"
+    floatingDiv2.classList.remove('hidden')
+    floatingDiv.classList.add('hidden')
   }
   
   const update3 = (d) => {
-  
+    if(chartType==3)return;
+    chartType=3;
+    let variable = "expend";
+
     floatingDiv.innerText = "3"
+    floatingDiv.classList.remove('hidden')
+    floatingDiv2.classList.add('hidden')
   }
   
+const update4 = (d) => {
+  if(chartType==4)return;
+  chartType=4;
+  let variable = "revenue per enroll"
+
+  floatingDiv2.innerText= "4";
+  floatingDiv2.classList.remove('hidden')
+  floatingDiv.classList.add('hidden')
+}
+
   let update = {
     0: update1,
     1: update2,
     2:update3,
-    3: update1
+    3: update4
   }
   
 const handleUpdate = (k) => {
+  console.log(chartType)
+  if(!k)return update[chartType-1]();
   if(-k>4 || -k<0)return;
   console.log(parseInt(-k*7)%7)
-  floatingDiv.style.top = `${(h2-h1)/7*(parseInt(-k*7)%7) + h1}px`;
+  floatingDiv.style.top = `${h2-(h2)/5*(parseInt(-k*7)%7)}px`;
+  floatingDiv2.style.top = `${h2-(h2)/5*(parseInt(-k*7)%7)}px`;
   let index = parseInt(-k); //floors k 
   let color = {
     0: 'green',
@@ -72,10 +119,14 @@ const handleUpdate = (k) => {
     3: 'orange'
   }
   svg.selectAll(".state").style('fill', color[index]);
-  update[index]();
+  if(chartType-1 !== index){
+    clearAnimator();
+    update[index](); 
+  }
 }
 
 const createVisualization = (d) => {
+  chartType=1;
     svg = d3.select("#main").append('svg')
     .attr('width', w)
     .attr('height', h);
@@ -103,7 +154,7 @@ const createVisualization = (d) => {
     // 5. Draw the map using SVG path elements, styling with fill values
     // from our color scale
 
-    let map = svg.append('g');
+    map = svg.append('g');
 
     //Bind data and create one path per GeoJSON feature
     map.selectAll(".state")
@@ -149,12 +200,65 @@ Promise.all([
       }
     });
 
-    let stateValues = new Map(states.map(d => [d.state, d.value]));
+    stateValues = new Map(states.map(d => [d.state, d.value]));
 
     console.log(stateValues);
     floatingDiv= document.querySelector('#floatingBlock');
     floatingDiv.style.top = `${h -200}px`;
+    floatingDiv2= document.querySelector('#floatingBlock2');
+    floatingDiv2.style.top = `${h -200}px`;
 
     createVisualization(stateData);
+
+    //animation for a slider from http://bl.ocks.org/darrenjaworski/5544599
+    button = document.querySelector("button");
+    let slider = document.querySelector("#slider")
+    button.addEventListener("click", () => {
+      let duration = 2000;
+      let max = 2015;
+      let min = 1993;
+
+      if(running){
+        button.innerHTML = "Play";
+        running = false;
+        clearInterval(timer);
+      }else{
+        button.innerHTML = "Pause";
+        year = slider.value;
+        timer = setInterval(() => {
+          if(year <max){
+            year++;
+          }else{
+            button.innerHTML = "Play";
+            running = false;
+            year = min;
+            clearInterval(timer);
+          }
+          document.querySelector("#range").innerHTML = year;
+          slider.value = year;
+          handleUpdate();
+
+        }, duration);
+        running= true;
+      }
+    })
+
+    slider.addEventListener("change", () =>{
+      handleUpdate();
+      document.querySelector("#range").innerHTML = slider.value;
+      clearInterval(timer);
+      running=false;
+      button.innerHTML="Play"
+    })
+
+    clearAnimator = () => {
+      
+      button.innerHTML = "Play";
+      running = false;
+      year = 1993;
+      clearInterval(timer);
+      document.querySelector("#range").innerHTML = year;
+      slider.value = year;
+    }
 
   }); 
